18420fe571d63595da64c8720cb949ed4e9dec35
Isaac Hunter <isaachunter@Isaacs-MacBook-Pro.local>
2019-04-19 23:42:16 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GroupsComponent } from './groups/groups.component';
+import { CreatorComponent } from './creator/creator.component';
+import { PoemListComponent } from './home/poem-list/poem-list.component';
+import { PoemDetailComponent } from './home/poem-detail/poem-detail.component';
+import { SideBarComponent } from './home/side-bar/side-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    GroupsComponent,
+    CreatorComponent,
+    PoemListComponent,
+    PoemDetailComponent,
+    SideBarComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
